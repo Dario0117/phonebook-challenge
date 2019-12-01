2f41b1ed8eab61399ba329467f80b27b753dc83a
Esteban D. Barboza Primera <dario0117@gmail.com>
2019-11-30 22:27:02 -0500

    fixed cors and resources consumed by front end

--- phoneboko-challenge-ui/src/App.js
+++ phoneboko-challenge-ui/src/App.js
@@ -1,26 +1,184 @@
 import React from 'react';
-import logo from './logo.svg';
+import phonebook from './phonebook.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    contacts: [],
+    searchTerm: "",
+    searchResult: [],
+    firstName: "",
+    lastName: "",
+    phone: "",
+  }
 
+  loadContacts = () => {
+    fetch("http://localhost:8080/contact")
+      .then(res => res.json())
+      .then(contacts => {
+        this.setState({
+          contacts
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.loadContacts()
+  }
+
+  handleSearch = (e) => {
+    e.preventDefault()
+    fetch("http://localhost:8080/contact/search", {
+      method: 'POST',headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: this.state.searchTerm
+      })
+    })
+    .then(res => res.json())
+    .then(contacts => {
+      this.setState({
+        searchResult: contacts
+      })
+    })
+  }
+
+  handleAdd = (e) => {
+    e.preventDefault()
+    fetch("http://localhost:8080/contact", {
+      method: 'POST',headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: this.state.firstName,
+        phone: this.state.phone,
+      })
+    })
+    .then(res => res.json())
+    .then(contacts => {
+      this.setState({
+        searchResult: contacts
+      })
+      this.loadContacts()
+    })
+  }
+
+  handleSearchInput = (e) => {
+    e.preventDefault()
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+
+  handleFirstName = (e) => {
+    e.preventDefault()
+    this.setState({
+      firstName: e.target.value
+    })
+  }
+
+  handleLastName = (e) => {
+    e.preventDefault()
+    this.setState({
+      lastName: e.target.value
+    })
+  }
+
+  handlePhone = (e) => {
+    e.preventDefault()
+    this.setState({
+      phone: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="pure-g">
+          <div className="pure-u-1">
+            <div className="header">
+              <img className="logo" alt="logo" src={phonebook} />
+              <p>v 1.0</p>
+            </div>
+          </div>
+        </div>
+        <div className="pure-g">
+          <div className="pure-u-sm-1 pure-u-1-3">
+            <div className="box">
+              <h2><i className="fa fa-user-plus"></i>New contact</h2>
+              <form className="pure-form">
+                <fieldset className="pure-group">
+                  <input type="text" className="pure-input-1-2" placeholder="First Name" value={this.state.firstName} onChange={this.handleFirstName} required />
+                  <input type="text" className="pure-input-1-2" placeholder="Last Name" value={this.state.lastName} onChange={this.handleLastName} />
+                  <input type="text" className="pure-input-1-2" placeholder="Phone" value={this.state.phone} onChange={this.handlePhone} required />
+                </fieldset>
+                <button onClick={this.handleAdd} type="submit" className="pure-button pure-input-1-2 pure-button-primary">
+                  <i className="fa fa-user-plus"></i>Add</button>
+              </form>
+            </div>
+          </div>
+          <div className="pure-u-sm-1 pure-u-1-3">
+            <div className="box">
+              <h2><i className="fa fa-search"></i>Search contact</h2>
+              <form className="pure-form">
+                <fieldset className="pure-group">
+                  <input type="text" className="pure-input-1-2" value={this.state.searchTerm} onChange={this.handleSearchInput} />
+                </fieldset>
+                <button onClick={this.handleSearch} type="submit" className="pure-button pure-input-1-2 pure-button-primary">
+                  <i className="fa fa-search"></i>Search</button>
+              </form>
+              {this.state.searchResult.length > 0 && (
+                <table className="pure-table">
+                <thead>
+                  <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Phone</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {this.state.searchResult.map(contact => (
+                    <tr key={contact.id}>
+                      <td>{contact.name}</td>
+                      <td>Kart</td>
+                      <td>{contact.phone}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              )}
+            </div>
+          </div>
+          <div className="pure-u-sm-1 pure-u-1-3">
+            <div className="box">
+              <h2><i className="fa fa-users"></i> Contacts</h2>
+              <table className="pure-table">
+                <thead>
+                  <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Phone</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {this.state.contacts.map(contact => (
+                    <tr key={contact.id}>
+                      <td>{contact.name}</td>
+                      <td>Kart</td>
+                      <td>{contact.phone}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+}
 export default App;
--- phonebook-challenge/src/main/java/com/dario0117/phonebookchallenge/controllers/ContactController.java
+++ phonebook-challenge/src/main/java/com/dario0117/phonebookchallenge/controllers/ContactController.java
@@ -5,13 +5,14 @@ import com.dario0117.phonebookchallenge.services.ContactService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/contact")
 public class ContactController {
